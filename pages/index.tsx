415fd8ce0c3304235afeaf7841537863979a41a3
Tomas <tmercadoslp@gmail.com>
2022-08-08 15:28:39 -0300

    chore: init proyect with create next app

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Flashcards</title>
+        <meta name="description" content="Flashcards study app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Welcome to Flashcards!</h1>
+    </div>
+  );
+};
+
+export default Home;
