6f73af66e83d0aa953ec0a8e871f191fa57f8086
Agustin Vazquez <agustinvazquez453@gmail.com>
2022-08-08 20:52:43 -0300

    fix: restore home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,41 +4,6 @@ import { ChangeEventHandler, FormEventHandler, useState } from "react";
 import { signUp } from "../firebase/auth";
 
 const Home: NextPage = () => {
-  const [state, setState] = useState({
-    email: "",
-    password: "",
-    repeatPassword: "",
-  });
-
-  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
-    const { name, value } = event.target;
-    setState({ ...state, [name]: value });
-  };
-
-  const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
-    e.preventDefault();
-
-    const { email, password, repeatPassword } = state;
-
-    if (!password || !repeatPassword || !email) {
-      window.alert("Please fill all fields");
-      return;
-    }
-
-    if (password !== repeatPassword) {
-      window.alert("Passwords do not match");
-      return;
-    }
-
-    const user = await signUp(email, password);
-
-    if (user) {
-      window.alert("User created");
-      console.log(user);
-    } else {
-      window.alert("User not created");
-    }
-  };
 
   return (
     <div>
@@ -49,40 +14,7 @@ const Home: NextPage = () => {
       </Head>
 
       <div>
-        <h1>Registrate</h1>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              onChange={handleChange}
-            />
-          </div>
-
-          <div>
-            <label htmlFor="password">Contraseña</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              onChange={handleChange}
-            />
-          </div>
-
-          <div>
-            <label htmlFor="repeatPassword">Repetir Contraseña</label>
-            <input
-              type="password"
-              id="repeatPassword"
-              name="repeatPassword"
-              onChange={handleChange}
-            />
-          </div>
-
-          <button type="submit">Registrate!</button>
-        </form>
+        <h1>Welcome to Flashcards!</h1>
       </div>
     </div>
   );
