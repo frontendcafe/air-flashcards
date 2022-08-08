99965d591cd426c7082f3a36047830e31a816ff9
Tomas <tmercadoslp@gmail.com>
2022-08-08 19:42:13 -0300

    feat(pages/index): add register form using firebase auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,45 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { ChangeEventHandler, FormEventHandler, useState } from "react";
+import { signUp } from "../firebase/auth";
 
 const Home: NextPage = () => {
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+    repeatPassword: "",
+  });
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const { name, value } = event.target;
+    setState({ ...state, [name]: value });
+  };
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+
+    const { email, password, repeatPassword } = state;
+
+    if (!password || !repeatPassword || !email) {
+      window.alert("Please fill all fields");
+      return;
+    }
+
+    if (password !== repeatPassword) {
+      window.alert("Passwords do not match");
+      return;
+    }
+
+    const user = await signUp(email, password);
+
+    if (user) {
+      window.alert("User created");
+      console.log(user);
+    } else {
+      window.alert("User not created");
+    }
+  };
+
   return (
     <div>
       <Head>
@@ -10,7 +48,42 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Welcome to Flashcards!</h1>
+      <div>
+        <h1>Registrate</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              onChange={handleChange}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="password">Contraseña</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              onChange={handleChange}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="repeatPassword">Repetir Contraseña</label>
+            <input
+              type="password"
+              id="repeatPassword"
+              name="repeatPassword"
+              onChange={handleChange}
+            />
+          </div>
+
+          <button type="submit">Registrate!</button>
+        </form>
+      </div>
     </div>
   );
 };
