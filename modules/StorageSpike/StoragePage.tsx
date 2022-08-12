28fad7df67ed529255ebb12580acdd916f79897a
Kevin Dev <kevin.developer71@gmail.com>
2022-08-11 21:45:57 -0600

    dummy form to upload an image to firebase

---
+++ modules/StorageSpike/StoragePage.tsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from "react";
+
+import { uploadToImagesFolder } from "@/firebase/utils/storage";
+
+import styles from "./StoragePage.module.scss";
+
+const StoragePage = () => {
+  const [imgUrl, setImgUrl] = useState(null);
+  const [progresspercent, setProgresspercent] = useState(0);
+  const [uploadError, setUploadError] = useState(null);
+  const [localTempImgUrl, setLocalTempImgUrl] = useState<string>("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const [eventTarget]: any = e.target;
+    const file = eventTarget.files?.[0];
+    if (!file) return;
+
+    uploadToImagesFolder({
+      file,
+      onProgress: (percent: any) => {
+        setProgresspercent(percent);
+      },
+      onCompleted: (downloadUrl: any) => {
+        setImgUrl(downloadUrl);
+      },
+      onError: (err: any) => {
+        setUploadError(err);
+      },
+    });
+  };
+
+  const onImageChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const eventTarget = e.target as HTMLInputElement;
+    const file = eventTarget.files?.[0];
+    if (!file) return;
+
+    setLocalTempImgUrl(URL.createObjectURL(file));
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        {uploadError && (
+          <div>
+            <h2>ERROR!</h2>
+            {uploadError}
+          </div>
+        )}
+        <div>
+          <form onSubmit={handleSubmit}>
+            <input type="file" onChange={onImageChange} accept="image/*" />
+            <button type="submit">Upload</button>
+          </form>
+          {!imgUrl && (
+            <div className="outerbar">
+              <div className="innerbar" style={{ width: `${progresspercent}%` }}>
+                {progresspercent}%
+              </div>
+            </div>
+          )}
+          {imgUrl && <img src={imgUrl} alt="uploaded file" height={200} />}
+        </div>
+        <div className={styles.previewWrapper}>
+          <div className={styles.item}>
+            <h3>Preview de tu imagen a subir</h3>
+            {localTempImgUrl && (
+              <img className={styles.previewImage} src={localTempImgUrl} alt="" />
+            )}
+          </div>
+          {imgUrl && (
+            <div className={styles.item}>
+              <h3>tu imagen se ha subido exitosamente</h3>
+              <span>URL: {imgUrl}</span>
+              {imgUrl && <img className={styles.previewImage} src={imgUrl} alt="" />}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StoragePage;
---
+++ pages/upload.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+import { StoragePage } from "@/modules/StorageSpike";
+
+const UploadPage: NextPage = () => {
+  return <StoragePage />;
+};
+
+export default UploadPage;
