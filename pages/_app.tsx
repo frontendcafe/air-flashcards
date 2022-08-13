d91520a44a805bd4845961707aedfde720ec07fe
MA0HS <oralia.hernandez@hotmail.com>
2022-08-13 12:11:24 -0500

    [FIX] firebaseConfig reference updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 
-import "@/modules/Auth/firebase";
+import "@/firebaseConfig";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
