9afe01f7f5a93c2e5d3309781ef46af02c417fae
Marcelo Nieva <marcelonieva@gmail.com>
2022-08-18 19:12:44 -0300

    feat(Api): add jsdoc to createDoc()

--- modules/Api/Services/FireStore/index.ts
+++ modules/Api/Services/FireStore/index.ts
@@ -2,6 +2,11 @@ import { addDoc, collection } from "firebase/firestore";
 
 import db from "@/modules/Firestore";
 
+/**
+ * Add a document to the collection specified.
+ * @param {string} collectionPath - Path of the collection.
+ * @param {DocData} docData - Document content.
+ */
 export async function createDoc<DocData>(collectionPath: string, docData: DocData) {
   const collRef = collection(db, collectionPath);
   const newDocRef = await addDoc(collRef, docData);
