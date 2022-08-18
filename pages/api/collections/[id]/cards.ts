559281809e4991f0301da7ec8ae9777089d9ec3c
Marcelo Nieva <marcelonieva@gmail.com>
2022-08-17 21:08:52 -0300

    feat(Cards): add fn to create to cards endpoint

--- pages/api/collections/[id]/cards.ts
+++ pages/api/collections/[id]/cards.ts
@@ -1,15 +1,18 @@
 import { NextApiHandler } from "next";
 import { collection, doc, getDoc, getDocs } from "firebase/firestore";
+import { ValidationError } from "yup";
 
+import { ClientError, ResponseContent } from "@/modules/Api/models";
+import { createDoc } from "@/modules/Api/Services/FireStore";
+import { checkAllowedMethods } from "@/modules/Api/utils";
 import { Card, CardData } from "@/modules/Cards/models";
+import { cardDataSchema } from "@/modules/Cards/schema";
 import db from "@/modules/Firestore";
-
-const allowedMethods = ["GET"];
 
 const getCollectionCards = async (collectionId: string) => {
   const collectionExists = await (await getDoc(doc(db, "collections", collectionId))).exists();
   if (!collectionExists) {
-    throw new Error("Collection not exists");
+    throw new ClientError("Collection not exists", 404);
   }
 
   const cardsSnapshot = await getDocs(collection(db, `collections/${collectionId}/cards`));
@@ -28,20 +31,42 @@ const getCollectionCards = async (collectionId: string) => {
   return cards;
 };
 
-const collectionCardsHandler: NextApiHandler = async (request, response) => {
-  if (!allowedMethods.includes(request.method || "")) {
-    return response.status(405).send("Method not supported");
-  }
-  const collectionId = request.query.id;
+const getCards: NextApiHandler<Card[]> = async (request, response) => {
+  const { id } = request.query;
+
+  const collectionId = id;
   if (!collectionId || Array.isArray(collectionId)) {
-    return response.status(400).send("collectionId must be an string");
+    throw new ClientError("collectionId must be an string", 400);
   }
 
+  const cards = await getCollectionCards(collectionId);
+  return response.json(cards);
+};
+
+const createCard: NextApiHandler<Card> = async (request, response) => {
+  const { query, body } = request;
+  const card = await cardDataSchema.validate(body);
+  const collectionPath = `collections/${query.id}/cards`;
+
+  const newCard = await createDoc<CardData>(collectionPath, card);
+  return response.json(newCard);
+};
+
+const collectionCardsHandler: NextApiHandler<ResponseContent<Card>> = async (request, response) => {
   try {
-    const cards = await getCollectionCards(collectionId);
-    return response.json(cards);
-  } catch (error: any) {
-    return response.status(404).send(error.message);
+    checkAllowedMethods(["GET", "POST"], request.method);
+
+    const methodHandler = {
+      GET: getCards,
+      POST: createCard,
+    };
+    const method = request.method as keyof typeof methodHandler;
+    await methodHandler[method](request, response);
+  } catch (error) {
+    let code = 500;
+    if (error instanceof ValidationError) code = 400;
+    if (error instanceof ClientError) code = error.code;
+    response.status(code).send(String(error));
   }
 };
 
