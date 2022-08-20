94b152f7ac8ce8fe0193be7b556f85b2ad860765
Carlos Marcelo Nieva <72712805+marcelonieva7@users.noreply.github.com>
2022-08-20 13:11:35 -0300

    [MAIN][FEATURE] Add Card model (#56)
    * feat(Card): add model
    
    * refactor(card): refactor card model
    
    Co-authored-by: Marcelo Nieva <marcelonieva@gmail.com>

--- modules/Cards/models.ts
+++ modules/Cards/models.ts
@@ -1,5 +1,15 @@
+export type CardeSideType = "text" | "image";
+
+interface CardSide {
+  type: CardeSideType;
+  value: string;
+}
+
 export interface CardData {
   name: string;
+  createdAt: string;
+  sideA: CardSide;
+  sideB: CardSide;
 }
 
 export interface Card extends CardData {
