66621aaf19ddbf49ab98ed3b1215823c1a1e4b19
MA0HS <oralia.hernandez@hotmail.com>
2022-08-20 19:54:08 -0500

    feature(StudySession/Services): add deleteStudySession function

--- modules/StudySession/services.ts
+++ modules/StudySession/services.ts
@@ -1,6 +1,6 @@
 import { StudySessionData } from "./models";
-import { collection, addDoc } from "firebase/firestore";
-import db from "@/air-flashcards/modules/Firestore";
+import { collection, addDoc, deleteDoc,doc } from "firebase/firestore";
+import db from "@/modules/Firestore";
 
 export interface CreateStudySessionData extends Omit<StudySessionData, "completed"> {
   collectionId: string;
@@ -14,4 +14,9 @@ export const createStudySession = async (data: CreateStudySessionData) => {
     cardsAmount: data.cardsAmount,
   });
   console.log("Document written with ID: ", docRef.id);
+};
+
+export const deleteStudySession = async(collectionId: string, StudySessionId: string)=>{
+  const StudySessionRef = doc(db,`collections/${collectionId}/studySessions/${StudySessionId}`);
+  await deleteDoc(StudySessionRef);
 };
