8984498ae5ca3605c6a1dc752389fac4361b50f7
MA0HS <oralia.hernandez@hotmail.com>
2022-08-20 19:55:12 -0500

    feature(api/studySession): added delete StudySession endpoint

--- pages/api/study-sessions/index.ts
+++ pages/api/study-sessions/index.ts
@@ -1,13 +1,13 @@
 import { NextApiHandler } from "next";
-import { createStudySession } from "@/modules/StudySession/services";
+import { createStudySession,deleteStudySession } from "@/modules/StudySession/services";
 
-const allowedMethods = ["POST"];
+const allowedMethods = ["GET", "PATCH", "DELETE","POST"];
 
 const StudySessionDetailHandler: NextApiHandler = async (request, response) => {
   if (!allowedMethods.includes(request.method || "")) {
     return response.status(405).send("Method not supported");
   }
-  const { collectionId, mode, date, cardsAmount } = request.body;
+  const { collectionId, mode, date, cardsAmount,StudySessionId } = request.body;
 
   if (!collectionId || Array.isArray(collectionId)) {
     return response.status(400).send("collectionId must be an string");
@@ -25,13 +25,25 @@ const StudySessionDetailHandler: NextApiHandler = async (request, response) => {
     return response.status(400).send("cardsAmount must be a number.");
   }
   try {
-    const studySession = await createStudySession({
-      mode: mode,
-      date: date,
-      cardsAmount: cardsAmount,
-      collectionId: collectionId,
-    });
-    return response.json(studySession);
+    switch (request.method) {
+      case "POST":
+        const studySession = await createStudySession({
+        mode: mode,
+        date: date,
+        cardsAmount: cardsAmount,
+        collectionId: collectionId,
+      });
+      return response.json(studySession);
+
+      case "DELETE":
+        await deleteStudySession(collectionId, StudySessionId);
+        return response.json({ message: "Study Session deleted successfully" });
+
+      default:
+        return new Error("Unhandled method");
+    }
+    
+
   } catch (error: any) {
     return response.status(404).send(error.message);
   }
