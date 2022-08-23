9020e19e61f74e0f1b96bc1ddc5e02ad2b2bf00b
Dafne Caneda <dafnenicolecaneda@gmail.com>
2022-08-22 21:39:31 -0300

    Create Study Sessions Services

--- modules/StudySession/services.ts
+++ modules/StudySession/services.ts
@@ -1,17 +1,32 @@
-import { StudySessionData } from "./models";
-import { collection, addDoc } from "firebase/firestore";
+import { CreateStudySessionData, StudySessionData } from "./models";
+import { collection, addDoc, updateDoc, doc } from "firebase/firestore";
 import db from "@/air-flashcards/modules/Firestore";
+import { stringLength } from "@firebase/util";
 
-export interface CreateStudySessionData extends Omit<StudySessionData, "completed"> {
-  collectionId: string;
-}
+const STUDY_SESSIONS_COLLECTION = "collections/LBz0PhXuEiPEyIy6JloU/studySessions";
 
 export const createStudySession = async (data: CreateStudySessionData) => {
-  const docRef = await addDoc(collection(db, `collections/${data.collectionId}/studySessions`), {
+  const docRef = await addDoc(collection(db, STUDY_SESSIONS_COLLECTION), {
     mode: data.mode,
     date: data.date,
     completed: false,
     cardsAmount: data.cardsAmount,
   });
   console.log("Document written with ID: ", docRef.id);
+  return docRef.id;
+};
+
+export interface UpdateStudySession extends Omit<StudySessionData, "completed"> {
+  sessionId: string;
+}
+
+export const updateStudySession = async (data: UpdateStudySession) => {
+  const sessionRef = doc(db, STUDY_SESSIONS_COLLECTION, data.sessionId);
+  await updateDoc(sessionRef, {
+    mode: data.mode,
+    date: data.date,
+    completed: false,
+    cardsAmount: data.cardsAmount,
+  });
+  console.log("actualizado", sessionRef.id);
 };
