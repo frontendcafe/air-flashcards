49f57b177c38aca49df758633bf84857966bdf88
Dafne Caneda <dafnenicolecaneda@gmail.com>
2022-08-22 21:39:02 -0300

    Created Utils directory and Yup Hook Form

---
+++ modules/utils/useFormWithYup.ts
@@ -0,0 +1,10 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+function useFormWithYup<T>(schema: Parameters<typeof yupResolver>[0]) {
+  return useForm<T>({
+    resolver: yupResolver(schema),
+  });
+}
+
+export default useFormWithYup;
