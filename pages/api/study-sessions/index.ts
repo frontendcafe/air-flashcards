913336cf6b04a224c26971120e717f29a8324357
MA0HS <oralia.hernandez@hotmail.com>
2022-08-22 19:32:52 -0500

    feat(api/studysession) fix handler structure

--- pages/api/study-sessions/index.ts
+++ pages/api/study-sessions/index.ts
@@ -1,19 +1,16 @@
 import { NextApiHandler } from "next";
 import { createStudySession,deleteStudySession } from "@/modules/StudySession/services";
 
-const allowedMethods = ["GET", "PATCH", "DELETE","POST"];
-
 const StudySessionDetailHandler: NextApiHandler = async (request, response) => {
-  if (!allowedMethods.includes(request.method || "")) {
-    return response.status(405).send("Method not supported");
-  }
+
   const { collectionId, mode, date, cardsAmount,StudySessionId } = request.body;
 
-  if (!collectionId || Array.isArray(collectionId)) {
+ if (request.method === "POST" || request.method === "PATCH" ){
+   if (!collectionId || Array.isArray(collectionId)) {
     return response.status(400).send("collectionId must be an string");
   }
 
-  if (mode !== "normal" || mode !== "jeopardy" || mode !== "combined") {
+  if (mode !== "normal" && mode !== "jeopardy" && mode !== "combined") {
     return response.status(400).send("Mode must be normal, jeopardy or combined.");
   }
 
@@ -24,6 +21,7 @@ const StudySessionDetailHandler: NextApiHandler = async (request, response) => {
   if (typeof cardsAmount !== "number") {
     return response.status(400).send("cardsAmount must be a number.");
   }
+ }
   try {
     switch (request.method) {
       case "POST":
@@ -38,14 +36,16 @@ const StudySessionDetailHandler: NextApiHandler = async (request, response) => {
       case "DELETE":
         await deleteStudySession(collectionId, StudySessionId);
         return response.json({ message: "Study Session deleted successfully" });
+        //TODO
+        //dejar error cuando sesion no existe, responder 404 error
 
       default:
-        return new Error("Unhandled method");
+        return response.status(405).send("Method not allowed");
     }
     
 
   } catch (error: any) {
-    return response.status(404).send(error.message);
+    return response.status(500).send(error.message);
   }
 };
 
