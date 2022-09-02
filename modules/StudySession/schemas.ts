9bc0ddd0f521c72c5d315f5b253736a4349ea8b8
Tomas <tmercadoslp@gmail.com>
2022-09-02 20:16:04 -0300

    feat(StudySession/schemas): use correct interface & add error messages

--- modules/StudySession/schemas.ts
+++ modules/StudySession/schemas.ts
@@ -1,9 +1,11 @@
-import { object, date, number, mixed, SchemaOf, string } from "yup";
-import { StudySessionMode, CreateStudySessionData } from "./models";
+import { mixed, number, object, SchemaOf, string } from "yup";
 
-export const formSchema: SchemaOf<CreateStudySessionData> = object({
-  date: string().required(),
+import { FormStudySessionData, StudySessionMode } from "./models";
+
+export const formSchema: SchemaOf<FormStudySessionData> = object({
   cardsAmount: number().required().min(0),
-  mode: mixed<StudySessionMode>().oneOf(Object.values(StudySessionMode)).required(),
-  collectionId: string().required(),
+  mode: mixed<StudySessionMode>()
+    .oneOf(Object.values(StudySessionMode), "Debes seleccionar un modo de juego")
+    .required("Debes seleccionar un modo de juego"),
+  collectionId: string().required("Debes seleccionar una colección"),
 });
