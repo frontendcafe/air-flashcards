300759d3a2a54be5010cf9015ac1b9fdfa1fedcb
Tomas <tmercadoslp@gmail.com>
2022-09-05 21:20:03 -0300

    feat(StudySession/GameCard): make component controlled

--- modules/StudySession/components/GameCard.tsx
+++ modules/StudySession/components/GameCard.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
-import { Box, Text } from "@chakra-ui/react";
+import React, { MouseEventHandler } from "react";
 import { motion } from "framer-motion";
+
+import { Box, Text } from "@chakra-ui/react";
 
 interface CardSide {
   image?: string;
@@ -10,75 +11,73 @@ interface CardSide {
 interface GameCardPops {
   sideA: CardSide;
   sideB: CardSide;
+  onClick: MouseEventHandler<HTMLDivElement>;
+  flipped: boolean;
 }
 
-export const GameCard: React.FC<GameCardPops> = ({ sideA, sideB }) => {
-  const [flip, setFlip] = useState(false);
+export const GameCard: React.FC<GameCardPops> = ({ sideA, sideB, flipped, onClick }) => {
   const MotionBox = motion(Box);
 
   return (
-    <>
-      <Box>
+    <Box>
+      <MotionBox
+        onClick={onClick}
+        borderRadius="10px"
+        w="full"
+        height="300px"
+        position="relative"
+        cursor="pointer"
+      >
         <MotionBox
-          onClick={() => setFlip(!flip)}
+          bg="white"
+          border="2px"
+          borderColor="gray.50"
+          position="absolute"
+          width="100%"
+          height="100%"
           borderRadius="10px"
-          width={{ base: "250px", md: "500px" }}
-          height="300px"
-          position="relative"
-          cursor="pointer"
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          style={{
+            backfaceVisibility: "hidden",
+          }}
+          animate={{ rotateY: flipped ? [0, 360] : [360, 0] }}
+          transition={{ duration: 1 }}
         >
-          <MotionBox
-            bg="white"
-            //
-            border="2px"
-            borderColor="gray.50"
-            position="absolute"
-            width="100%"
-            height="100%"
-            borderRadius="10px"
-            display="flex"
-            flexDirection="column"
-            justifyContent="center"
-            alignItems="center"
-            //
-            style={{
-              backfaceVisibility: "hidden",
-            }}
-            animate={{ rotateY: flip ? [0, 360] : [360, 0] }}
-            transition={{ duration: 1 }}
-          >
-            {sideA.image && <img src={sideA.image} width={150} height={150} />}
-            <Text mt={10} fontWeight={600}>
-              {sideA.text}
-            </Text>
-          </MotionBox>
-          <MotionBox
-            bg="white"
-            //
-            border="2px"
-            borderColor="gray.50"
-            position="absolute"
-            width="100%"
-            height="100%"
-            borderRadius="10px"
-            display="flex"
-            flexDirection="column"
-            justifyContent="center"
-            alignItems="center"
-            //
-            style={{
-              backfaceVisibility: "hidden",
-            }}
-            animate={{ rotateY: flip ? [180, 0] : [0, 180] }}
-            transition={{ duration: 1 }}
-          >
-            {sideB.image && <img src={sideB.image} width={150} height={150} />}
-            <Text mt={10} fontWeight={600}>
-              {sideB.text}
-            </Text>
-          </MotionBox>
+          {sideA.image && <img src={sideA.image} width={150} height={150} />}
+          <Text mt={10} fontWeight={600}>
+            {sideA.text}
+          </Text>
         </MotionBox>
-      </Box>
-    </>
+
+        <MotionBox
+          bg="white"
+          //
+          border="2px"
+          borderColor="gray.50"
+          position="absolute"
+          width="100%"
+          height="100%"
+          borderRadius="10px"
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          //
+          style={{
+            backfaceVisibility: "hidden",
+          }}
+          animate={{ rotateY: flipped ? [180, 0] : [0, 180] }}
+          transition={{ duration: 1 }}
+        >
+          {sideB.image && <img src={sideB.image} width={150} height={150} />}
+          <Text mt={10} fontWeight={600}>
+            {sideB.text}
+          </Text>
+        </MotionBox>
+      </MotionBox>
+    </Box>
   );
 };
