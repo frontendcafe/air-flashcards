679c795fb7e3f82555181fc58d3af99457ab9ba2
nicodeheza <nicolasdeheza@hotmail.com>
2022-09-07 19:19:16 -0300

    bug(menu): eslint issues

--- modules/shared/components/Menu/Menu.tsx
+++ modules/shared/components/Menu/Menu.tsx
@@ -1,18 +1,20 @@
 import React from "react";
+import Link from "next/link";
+
 import {
+  Box,
   Drawer,
   DrawerBody,
-  DrawerOverlay,
-  DrawerContent,
   DrawerCloseButton,
-  useDisclosure,
-  Text,
+  DrawerContent,
+  DrawerOverlay,
   Flex,
-  Box,
+  Text,
+  useDisclosure,
 } from "@chakra-ui/react";
-import MenuLogo from "../Icons/MenuLogo";
+
 import Hamburguer from "../Icons/Hamburguer";
-import Link from "next/link";
+import MenuLogo from "../Icons/MenuLogo";
 
 const Links = [
   { title: "Colecciones", href: "/Collection" },
@@ -21,42 +23,44 @@ const Links = [
   { title: "Perfil", href: "/Perfil" },
 ];
 
-export default function Menu() {
+const Menu = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <>
-        <Flex justify="space-between" p={5}>
-          <Box>
-            <MenuLogo fill="white" width={18} height={23} borderColor="primary.100" borderWidth={7} />
-          </Box>
-          <Box onClick={onOpen} cursor="pointer">
-            <Hamburguer />
-          </Box>
-        </Flex>
+      <Flex justify="space-between" p={5}>
+        <Box>
+          <MenuLogo fill="white" width={18} height={23} borderColor="primary.100" borderWidth={7} />
+        </Box>
+        <Box onClick={onOpen} cursor="pointer">
+          <Hamburguer />
+        </Box>
+      </Flex>
 
-        <Drawer onClose={onClose} isOpen={isOpen} size={{ base: "full", md: "sm" }}>
-          <DrawerOverlay />
-          <DrawerContent bg="primary.100">
-            <DrawerCloseButton color="white" />
-            <DrawerBody display="flex" flexDirection="column" marginTop="120px" alignItems="center">
-              <MenuLogo
-                fill="white"
-                width={63}
-                height={83}
-                borderColor="primary.100"
-                borderWidth={2}
-              />
-              {Links.map((link) => (
-                <Text variant="menu">
-                  <Link href={link.href}>
-                    <a>{link.title}</a>
-                  </Link>
+      <Drawer onClose={onClose} isOpen={isOpen} size={{ base: "full", md: "sm" }}>
+        <DrawerOverlay />
+        <DrawerContent bg="primary.100">
+          <DrawerCloseButton color="white" />
+          <DrawerBody display="flex" flexDirection="column" marginTop="120px" alignItems="center">
+            <MenuLogo
+              fill="white"
+              width={63}
+              height={83}
+              borderColor="primary.100"
+              borderWidth={2}
+            />
+            {Links.map((link) => {
+              return (
+                <Text variant="menu" key={link.href}>
+                  <Link href={link.href}>{link.title}</Link>
                 </Text>
-              ))}
-            </DrawerBody>
-          </DrawerContent>
-        </Drawer>
+              );
+            })}
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </>
   );
-}
+};
+
+export default Menu;
