8dad78074dfa41d9c5acbbd0bd4995e8298fc822
Luciano Mariotti <85541756+mariodev9@users.noreply.github.com>
2022-09-15 01:15:33 -0300

    Update index.tsx

--- pages/onboarding/index.tsx
+++ pages/onboarding/index.tsx
@@ -1,8 +1,5 @@
-import type { GetServerSideProps, NextPage } from "next";
+import type { NextPage } from "next";
 import Head from "next/head";
-import nookies from "nookies";
-
-import { verifyIdToken } from "@/firebaseAdminConfig";
 import OnboardingBody from "@/modules/shared/components/Onboarding/Onboarding";
 
 const Onboarding: NextPage = () => {
@@ -18,23 +15,6 @@ const Onboarding: NextPage = () => {
       </div>
     </>
   );
-};
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  try {
-    const cookies = nookies.get(context);
-    const token = await verifyIdToken(cookies.token);
-    const { uid } = token;
-    // eslint-disable-next-line no-console
-    console.log({ uid });
-  } catch (error: any) {
-    // TODO
-  }
-  return {
-    props: {
-      requiresAuthentication: true,
-    },
-  };
 };
 
 Onboarding.requiresAuthentication = true;
