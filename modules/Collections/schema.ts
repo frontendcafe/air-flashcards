f5731d98e40c22a3c4474cb27347494a7c91e168
Agustin Vazquez <agustin.vazquez@treinta.co>
2022-09-19 20:12:19 -0300

    fix ts error

--- modules/Collections/schema.ts
+++ modules/Collections/schema.ts
@@ -8,7 +8,7 @@ export const createCollectionSchema = object({
   description: string(),
   userId: string().required(),
   cards: array().of(cardDataSchema),
-  tags: array().of(string()),
+  tags: array().of(string().required()),
 })
   .noUnknown(true)
   .strict(true);
