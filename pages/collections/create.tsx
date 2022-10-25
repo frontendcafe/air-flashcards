07091e7953120324a6b41e58da4ce8c3376053e7
Agustin Vazquez <agustin.vazquez@treinta.co>
2022-10-25 18:04:32 -0300

    add menu to all pages

--- pages/collections/create.tsx
+++ pages/collections/create.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 
 import CollectionFormsView from "@/modules/Collections/components/CollectionFormsView";
+import Menu from "@/modules/shared/components/Menu/Menu";
+import { Flex } from "@chakra-ui/react";
 
 const CreateCollection: React.FC = () => {
-  return <CollectionFormsView />;
+  return (
+    <div>
+      <Flex align="center" flexDir="column" maxH="100vh" overflowX="hidden">
+        <Menu />
+        <CollectionFormsView />
+      </Flex>
+    </div>
+  );
 };
 
 export default CreateCollection;
